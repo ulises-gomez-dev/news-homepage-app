c529b4a46b1e756084765377115022007f4a62a2
Ulises <ulises.gomez.dev@proton.me>
2025-07-05 21:53:27 -0700

    fixed excess spacing with in the Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ function Hero({ className }) {
         <h1>The Bright Future of Web 3.0?</h1>
         <p>
           We dive into the next evolution of the web that claims to put the
-          power of the platforms back in the hands of the people. But is it
+          power of the platforms back into the hands of the people. But is it
           really fulfilling its promise?
         </p>
         <button className="hero-btn">READ MORE</button>
